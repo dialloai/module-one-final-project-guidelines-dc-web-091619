fb6ffe1a7ef1ecefd9dda1df31bb6838220219da
github username <github email address>
2019-10-04 11:36:39 -0400

    README and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,17 +24,38 @@ g_street = Neighborhood.find_or_create_by(location: "G Street")
 space_1a = Parking.find_or_create_by(parking_space: "1A", vacancy: true, cost: 20, neighborhood_id: north_dupont.id)
 space_2a = Parking.find_or_create_by(parking_space: "2A", vacancy: false, cost: 20, neighborhood_id: north_dupont.id)
 space_3a = Parking.find_or_create_by(parking_space: "3A", vacancy: true, cost: 20, neighborhood_id: north_dupont.id)
+space_4a=  Parking.find_or_create_by(parking_space: "4A", vacancy: true, cost: 20, neighborhood_id: north_dupont.id)
+space_5a=  Parking.find_or_create_by(parking_space: "5A", vacancy: true, cost: 20, neighborhood_id: north_dupont.id)
+space_6a = Parking.find_or_create_by(parking_space: "6A", vacancy: true, cost: 20, neighborhood_id: north_dupont.id)
+space_7a = Parking.find_or_create_by(parking_space: "7A", vacancy: false, cost: 20, neighborhood_id: north_dupont.id)
+space_8a = Parking.find_or_create_by(parking_space: "8A", vacancy: true, cost: 20, neighborhood_id: north_dupont.id)
+space_9a=  Parking.find_or_create_by(parking_space: "9A", vacancy: true, cost: 20, neighborhood_id: north_dupont.id)
+space_10a= Parking.find_or_create_by(parking_space: "10A", vacancy: true, cost: 20, neighborhood_id: north_dupont.id)
 
 #south_dupont parkings
 space_1b = Parking.find_or_create_by(parking_space: "1B", vacancy: true, cost: 22, neighborhood_id: south_dupont.id)
 space_2b = Parking.find_or_create_by(parking_space: "2B", vacancy: true, cost: 22, neighborhood_id: south_dupont.id)
 space_3b = Parking.find_or_create_by(parking_space: "3B", vacancy: true, cost: 22, neighborhood_id: south_dupont.id)
+space_4b = Parking.find_or_create_by(parking_space: "4B", vacancy: false, cost: 22, neighborhood_id: south_dupont.id)
+space_5b = Parking.find_or_create_by(parking_space: "5B", vacancy: true, cost:22, neighborhood_id: south_dupont.id)
+space_6b = Parking.find_or_create_by(parking_space: "6B", vacancy: true, cost: 22, neighborhood_id: south_dupont.id)
+space_7b = Parking.find_or_create_by(parking_space: "7B", vacancy: true, cost: 22, neighborhood_id: south_dupont.id)
+space_8b = Parking.find_or_create_by(parking_space: "8B", vacancy: true, cost: 22, neighborhood_id: south_dupont.id)
+space_9b = Parking.find_or_create_by(parking_space: "9B", vacancy: false, cost: 22, neighborhood_id: south_dupont.id)
+space_10b = Parking.find_or_create_by(parking_space: "10B", vacancy: true, cost:22, neighborhood_id: south_dupont.id)
+
 
 #g_sreet parkings
 space_1c = Parking.find_or_create_by(parking_space: "1C", vacancy: true, cost: 17, neighborhood_id: g_street.id)
-space_2c = Parking.find_or_create_by(parking_space: "2C", vacancy: true, cost: 17, neighborhood_id: g_street.id)
-space_3c = Parking.find_or_create_by(parking_space: "3C", vacancy: true, cost: 17, neighborhood_id: g_street.id)
-
+space_2c = Parking.find_or_create_by(parking_space: "2C", vacancy: false, cost: 17, neighborhood_id: g_street.id)
+space_3c = Parking.find_or_create_by(parking_space: "3C", vacancy: false, cost: 17, neighborhood_id: g_street.id)
+space_4c = Parking.find_or_create_by(parking_space: "4C", vacancy: false, cost: 17, neighborhood_id: g_street.id)
+space_5c = Parking.find_or_create_by(parking_space: "5C", vacancy: false, cost: 17, neighborhood_id: g_street.id)
+space_6c = Parking.find_or_create_by(parking_space: "6C", vacancy: false, cost: 17, neighborhood_id: g_street.id)
+space_7c = Parking.find_or_create_by(parking_space: "7C", vacancy: false, cost: 17, neighborhood_id: g_street.id)
+space_8c = Parking.find_or_create_by(parking_space: "8C", vacancy: false, cost: 17, neighborhood_id: g_street.id)
+space_9c = Parking.find_or_create_by(parking_space: "9C", vacancy: false, cost: 17, neighborhood_id: g_street.id)
+space_10c = Parking.find_or_create_by(parking_space: "10C", vacancy: false, cost: 17, neighborhood_id: g_street.id)
 
 #####reservations
 tom_1a = Reservation.find_or_create_by(customer_id: tom.id, parking_id: space_2a.id)
